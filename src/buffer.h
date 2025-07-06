238f9a9b975ab97195b0982a08c8939e4eee2251
Luna <luna@luna4.xyz>
2025-07-06 14:27:25 +0200

    made buffer more robust and supports unsigned char buffers

--- src/buffer.h
+++ src/buffer.h
@@ -13,8 +13,26 @@ struct buffer
 
 	void write(char *data_in, size_t data_size);
 	void remove(int offset, int remove_size);
+	void allocate(size_t s);
 
 	~buffer()
+	{
+		free(data);
+	}
+};
+
+struct buffer_unsigned
+{
+	unsigned char *data;
+	size_t size;
+	size_t allocated;
+	int allocations;
+
+	void write(unsigned char *data_in, size_t data_size);
+	void remove(int offset, int remove_size);
+	void allocate(size_t s);
+
+	~buffer_unsigned()
 	{
 		free(data);
 	}
