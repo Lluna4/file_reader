27ba3791017f33bb60cc9775c2d8a5a44f400a41
luna <luna@luna4.xyz>
2025-07-25 05:40:26 +0200

    Adapted for unsigned char buffers

--- src/file_deserialising.h
+++ src/file_deserialising.h
@@ -29,18 +29,41 @@
 
 namespace file_reader
 {
+	template <typename T>
 	struct parsing_buffer
 	{
-		parsing_buffer(buffer<char> &b)
+		parsing_buffer(buffer<T> &b)
 		:buf(b)
 		{}
-		buffer<char> &buf;
-		char *point;
+		buffer<T> &buf;
+		T *point;
 		int consumed_size;
 	};
 
 	template <typename T>
 	static void read_type(T& val, char *data)
+	{
+		std::memcpy(&val, data, sizeof(T));
+		switch (sizeof(T))
+		{
+			case 1:
+				break;
+			case 2:
+				val = be16toh(val);
+				break;
+			case 4:
+				val = be32toh(val);
+				break;
+			case 8:
+				val = be64toh(val);
+				break;
+			default:
+				std::println("This integer is not supported!");
+		}
+	}
+
+	template <typename T>
+	static void read_type(T& val, unsigned char *data)
 	{
 		std::memcpy(&val, data, sizeof(T));
 		switch (sizeof(T))
@@ -72,7 +95,27 @@ namespace file_reader
 	}
 
 	template <>
+	inline void read_type<float>(float &val, unsigned char *data)
+	{
+		uint32_t num_as_uint32;
+
+		memcpy(&num_as_uint32, data, sizeof(float));
+		num_as_uint32 = be32toh(num_as_uint32);
+		memcpy(&val, &num_as_uint32, sizeof(float));
+	}
+
+	template <>
 	inline void read_type<double>(double &val, char *data)
+	{
+		uint64_t num_as_uint64;
+
+		memcpy(&num_as_uint64, data, sizeof(double));
+		num_as_uint64 = be64toh(num_as_uint64);
+		memcpy(&val, &num_as_uint64, sizeof(double));
+	}
+
+	template <>
+	inline void read_type<double>(double &val, unsigned char *data)
 	{
 		uint64_t num_as_uint64;
 
@@ -92,7 +135,7 @@ namespace file_reader
 	template<typename T>
 	struct read_var
 	{
-		static void call(T& val, parsing_buffer* v)
+		static void call(T& val, parsing_buffer<char> * v)
 		{
 			int size = 0;
 			if constexpr (typeid(T) == typeid(buffer<char>))
@@ -102,6 +145,26 @@ namespace file_reader
 			if (size + v->consumed_size > v->buf.size)
 			{
 				if constexpr (typeid(T) == typeid(buffer<char>))
+				{
+					val.size = v->buf.size;
+				}
+				else
+					return ;
+			}
+			read_type<T>(val, v->point);
+			v->point += size;
+			v->consumed_size += size;
+		}
+		static void call(T& val, parsing_buffer<unsigned char> * v)
+		{
+			int size = 0;
+			if constexpr (typeid(T) == typeid(buffer<unsigned char>))
+				size = val.size;
+			else
+				size = sizeof(T);
+			if (size + v->consumed_size > v->buf.size)
+			{
+				if constexpr (typeid(T) == typeid(buffer<unsigned char>))
 				{
 					val.size = v->buf.size;
 				}
